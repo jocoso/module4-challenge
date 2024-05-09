ff2b2991d35f824829bd429b410b7a0ea60264c8
jocoso <joshua.collado022@gmail.com>
2024-05-09 18:01:20 -0400

    Added comments and other qol additions to the code to make it more readable.

--- develop/assets/js/form.js
+++ develop/assets/js/form.js
@@ -1,22 +1,28 @@
 $(document).ready( function() {
+    // When the user submit the form
     $('#cool-form').on("submit", function (event) {
-        event.preventDefault();
-
+        event.preventDefault(); 
 
         if(localStorage.getItem("blogs") == null) {
-            var cal = localStorage.setItem("blogs", JSON.stringify([]));
+            // If the blog item doesn't exists in the localStorage we create an empty array
+            var cal = localStorage.setItem("blogs", JSON.stringify([])); 
         }
         
+        // Get raw blogs data 
         var blogs = JSON.parse(localStorage.getItem("blogs")); 
-        var ent = $('form').serializeArray();
+        var ent = $('form').serializeArray(); // Turn the data into an object
         
+        // I didn't like the way the function serializeArray formats their data so I
+        // Use it to create another, more organized object, to push to blogs
         blogs.push({name: ent[0].value, title: ent[1].value, content: ent[2].value});
         
+        // Flush the data
         localStorage.setItem("blogs", JSON.stringify(blogs));
 
+        // Empty the form 
         $('#cool-form')[0].reset();
 
-
+        // Kick the user to the blog
         window.location.href = "https://jocoso.github.io/module4-challenge/blog.html";
         
     })
--- develop/assets/js/logic.js
+++ develop/assets/js/logic.js
@@ -1,32 +1,60 @@
+// This file is used mostly for the toggle light button to 
+// change the classes of given object from dark to light and viceversa
+
 $( ".light-icon" ).on("click", function() {
+    // LIGHT2DARK
+    // --------
+
+    // BODY
     if( $( "body" ).hasClass( "dark" )) {
         $( "body" ).removeClass( "dark" );
         $( ".light-icon" ).text( "OFF" );
+
+        // Traverse through everything in the website
         $("*").each(function() {
+            // GLOBAL BORDERS
             if($(this).hasClass("light-box")) {
                 $(this).removeClass("light-box");
                 $(this).addClass("dark-box");
             }
+
+            // CARD DECORATIONS
             if($(this).hasClass("solid-white-bottom")) {
                 $(this).removeClass("solid-white-bottom");
                 $(this).addClass("solid-black-bottom");
             }
+            // IMPORTANT BUTTONS
+            if($(this).hasClass("light-button")) {
+                $(this).removeClass("light-button");
+                $(this).addClass("dark-button");
+            }
         });
     } else {
+        // DARK2LIGHT
+        // --------
+
+        // BODY
         $( "body" ).addClass( "dark" );
         $( ".light-icon" ).text( "ON" );
 
+        // Traverse through everything in the website
         $("*").each(function() {
-            if($(this).hasClass("dark-box")){
 
+            // GLOBAL BORDERS
+            if($(this).hasClass("dark-box")){
                 $(this).removeClass("dark-box");
                 $(this).addClass("light-box");
             }
+            // CARD DECORATIONS
             if($(this).hasClass("solid-black-bottom")) {
                 $(this).removeClass("solid-black-bottom");
                 $(this).addClass("solid-white-bottom");
             }
-            
+            // IMPORTANT BUTTONS
+            if($(this).hasClass("dark-button")) {
+                $(this).removeClass("dark-button");
+                $(this).addClass("light-button");
+            }
         })
 
     }
