63319406b65b2006a715e2d4262bfae734205795
jocoso <joshua.collado022@gmail.com>
2024-05-09 11:20:33 -0400

    Added the jquery file to a lib folder so it can be interpreted by google page

--- develop/assets/js/logic.js
+++ develop/assets/js/logic.js
@@ -2,7 +2,7 @@ $( ".light-icon" ).on("click", function() {
     if( $( "body" ).hasClass( "dark" )) {
         $( "body" ).removeClass( "dark" );
         $( ".light-icon" ).text( "OFF" );
-        $("*").each(function(item) {
+        $("*").each(function() {
             if($(this).hasClass("light-box")) {
                 $(this).removeClass("light-box");
                 $(this).addClass("dark-box");
@@ -12,7 +12,7 @@ $( ".light-icon" ).on("click", function() {
         $( "body" ).addClass( "dark" );
         $( ".light-icon" ).text( "ON" );
 
-        $("*").each(function(item) {
+        $("*").each(function() {
             if($(this).hasClass("dark-box")){
 
                 $(this).removeClass("dark-box");
