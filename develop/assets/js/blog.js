d51db42581ce603893de5038952bfffbb662b083
jocoso <joshua.collado022@gmail.com>
2024-05-09 17:00:40 -0400

    Now blogs can be seen and recorded by localStorage. A button to go from blog to index page was also implemented

--- develop/assets/js/blog.js
+++ develop/assets/js/blog.js
@@ -1,3 +1,19 @@
-$(document).isReady(function () {
-    var blogs;
+$(document).ready(function () {
+    var blogs = JSON.parse(localStorage.getItem("blogs") || "[]");
+
+
+    blogs.forEach(function (blogEntry) {
+        var entry = "<div class='card'> \
+        <span class='title solid-black-bottom'>"+ blogEntry.title + "</span>\
+        <pre class='content'>" + blogEntry.content + "</pre>\
+        <span class='author'>Posted by:"+ blogEntry.name + "</span>\
+    </div>";
+
+
+        $('#entries').append(entry);
+    })
 });
+
+function goBack() {
+    window.location = "/index.html";
+}
